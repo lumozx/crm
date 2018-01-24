287dc3ad168f3c8121d0609a91dc8b27623ca450
lumozx <969671691@qq.com>
2018-01-24 15:31:18 +0800

    修复表单会继承居中

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   width: 100%;
   height: 100%;
   min-width: 1024px !important;
