573e308f9ca8e2288c55815424cd8c666f883109
lumozx <969671691@qq.com>
2018-01-24 15:32:15 +0800

    更新数据

--- src/components/a_goods.vue
+++ src/components/a_goods.vue
@@ -2,27 +2,33 @@
 
   
   <el-container>
-<!--     <el-header style="text-align: right; font-size: 12px">
-      <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>查看</el-dropdown-item>
-          <el-dropdown-item>新增</el-dropdown-item>
-          <el-dropdown-item>删除</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <span>王小虎</span>
-    </el-header> -->
     <el-main>
+
+
+
+<el-form :inline="true"   class="demo-form-inline">
+
+  <el-form-item>
+      <el-input v-model="tableDataName" placeholder="请输入要搜索的信息" style="width:240px"></el-input>
+  </el-form-item>
+  <el-form-item>
+ <el-button type="primary" @click="doFilter">搜索</el-button>
+  </el-form-item>
+</el-form>
+
+
       <el-table :data="tableData.slice((currentPage-1)*thistotal,currentPage*thistotal)" @sort-change="sort_change">
         <el-table-column sortable="custom" prop="id" label="ID" width="140">
         </el-table-column>
-        <el-table-column prop="date" label="日期" width="140">
+        <el-table-column sortable="custom" prop="date" label="日期" width="140">
         </el-table-column>
         <el-table-column prop="name" label="姓名" width="120">
         </el-table-column>
+            <el-table-column prop="num" label="数量">
+        </el-table-column>
         <el-table-column prop="address" label="地址">
         </el-table-column>
+
       </el-table>
     <el-pagination
       @current-change="handleCurrentChange"
@@ -44,20 +50,25 @@ export default {
       id:'1',
         date: '',
         name: '',
-        address: ''
+        address: '',
+
+
       };
     return {
       tableData: Array(1).fill(item),
      // tableData:'',
      thistotal:15,//每页条数
      currentPage:1,//默认起始页
-     all:15//总条数
+     all:15,//总条数
+     tableDataName:''
     }
   },
   mounted(){
      this.$http.post('/api/s_goods').then((r)=>{
        this.tableData = r.data.table.list;
-        this.all=r.data.table.list.length
+       this.beiDate = [...r.data.table.list];//完全复制，消除指针，用于复制
+        this.all=r.data.table.list.length;
+        this.beiall=r.data.table.list.length;
      })
   },
   methods:{
@@ -66,14 +77,65 @@ export default {
 
       },
       sort_change(column, prop, order){
-        this.tableData = this.tableData.reverse()
-    
-      }
+        if(column.column){
+          this.tableData = this.tableData.sort(this.compare(column.prop,column.order))
+        }else{
+          this.tableData = this.tableData.sort(this.compare('id','ascending'))//默认id升序
+
+        }
+      },
+      compare(property,order){
+        if(property=='id'){
+         return function(obj1,obj2){
+            var value1 = obj1[property];
+            var value2 = obj2[property];
+            if(order=='ascending'){
+            return value1 - value2;     // 升序
+          }else if(order=='descending'){
+            return value2 - value1;     //倒序
+          }
+         }
+        }else if(property == 'date'){
+          return function(obj1,obj2){
+            var value1 = Date.parse(obj1[property]);
+            var value2 = Date.parse(obj2[property]);
+            if(order=='ascending'){
+            return value1 - value2;     // 升序
+          }else if(order=='descending'){
+            return value2 - value1;     //倒序
+          }
+         }
+        }
+    },
+    doFilter() {
+  if (this.tableDataName == "") {
+   this.tableData = [...this.beiDate];//完全复制，消除指针
+   this.all = this.beiall;
+  return;
   }
+  this.tableData = [...this.beiDate];//变换搜索条件的时候，要更新数据
+  this.filterTableDataEnd=[];
+  this.tableData.forEach((value, index) => {
+    for(let p in value){
+   if(value[p].toString().toUpperCase().indexOf(this.tableDataName.toUpperCase())>=0){//大小写模糊查询
+   this.filterTableDataEnd.push(value);
+   break;//防止多次检索同一条
+   }
+    }
+  });
+this.tableData = [...this.filterTableDataEnd];
+this.all = this.filterTableDataEnd.length;
+ },
+  },
+ 
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.demo-form-inline{
+  float: right;
+}
 </style>
+
